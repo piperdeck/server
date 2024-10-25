d5d42a3ac5feda9df81926057c788c4034edcfc8
Maxence Lange <maxence@artificial-owl.com>
2024-10-25 11:19:05 -0100

    feat(core): setting NCT namespace
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/composer/composer/autoload_psr4.php
+++ lib/composer/composer/autoload_psr4.php
@@ -9,5 +9,6 @@ return array(
     'OC\\Core\\' => array($baseDir . '/core'),
     'OC\\' => array($baseDir . '/lib/private'),
     'OCP\\' => array($baseDir . '/lib/public'),
+    'NCT\\' => array($baseDir . '/lib/testing'),
     '' => array($baseDir . '/lib/private/legacy'),
 );
